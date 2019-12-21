f2dd713973a7f3cf1e9c11a5b754fd288fb05b75
melodyfff <307208327@qq.com>
2019-12-21 22:56:05 +0800

    :smile: update

--- httpclient/src/main/java/com/xinchen/tool/httpclinet/spring/HttpClientConfiguration.java
+++ httpclient/src/main/java/com/xinchen/tool/httpclinet/spring/HttpClientConfiguration.java
@@ -1,5 +1,8 @@
 package com.xinchen.tool.httpclinet.spring;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -31,10 +34,14 @@ public class HttpClientConfiguration {
     @Value("${http.socketTimeout}")
     private Integer socketTimeout;
 
-    @Value("${http.staleConnectionCheckEnabled}")
-    private boolean staleConnectionCheckEnabled;
+    @Value("${http.validateAfterInactivity}")
+    private Integer validateAfterInactivity;
 
 
+    /**
+     * 实例化一个连接池管理器，设置最大连接数、并发连接数
+     * @return PoolingHttpClientConnectionManager
+     */
     @Bean
     public PoolingHttpClientConnectionManager httpClientConnectionManager(){
         PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
@@ -42,10 +49,36 @@ public class HttpClientConfiguration {
         cm.setMaxTotal(maxTotal);
         // 并发数
         cm.setDefaultMaxPerRoute(defaultMaxPerRoute);
+        cm.setValidateAfterInactivity(validateAfterInactivity);
         return cm;
     }
 
 
+    /**
+     * 注入连接池，用于获取httpClient
+     * @param httpClientConnectionManager PoolingHttpClientConnectionManager
+     *
+     * @return CloseableHttpClient
+     */
+    @Bean
+    public CloseableHttpClient closeableHttpClient(PoolingHttpClientConnectionManager httpClientConnectionManager){
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        httpClientBuilder.setConnectionManager(httpClientConnectionManager);
+        return httpClientBuilder.build();
+    }
+
+    /**
+     * 主要是发起 get/post请求时设置默认的超时时间等
+     * @return RequestConfig
+     */
+    @Bean
+    public RequestConfig getRequestConfig(){
+        RequestConfig.Builder builder = RequestConfig.custom();
+        return builder.setConnectTimeout(connectTimeout)
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
+                .build();
+    }
 
 
 }
