e358c0ee6023574d2443c825a139fb8cd0423d48
melodyfff <307208327@qq.com>
2020-01-31 11:53:18 +0800

    :smile: update

--- io/src/main/java/com/xinchen/tool/io/FileMan.java
+++ io/src/main/java/com/xinchen/tool/io/FileMan.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -21,32 +23,68 @@ import java.util.Objects;
 public class FileMan {
 
     /**
-     * 遍历文件路径，获取所有json文件
+     * 遍历文件路径，获取所有指定后缀的文件 ，如 .json .jpg .txt
      * @param filePath filePath文件路径
-     * @return List<String> json文件集合
+     * @param suffix 要搜寻的文件后缀
+     * @return List<String> 指定后缀文件字符串集合
      */
-    public static List<String> getJsonFilePath(String filePath) {
+    public static List<String> getFilePathStringsBySuffix(String filePath,String suffix) {
         File file = new File(filePath);
         List<String> pathList = new ArrayList<>();
-        getJsonFilePath(file, pathList);
+        getFilePathStringsBySuffix(file, pathList,suffix);
         return pathList;
     }
 
     /**
-     * 遍历文件路径，获取所有json文件
-     * @param file file
-     * @param pathList 返回查询到的文件列表
+     * 遍历文件路径，获取所有指定后缀的文件 ，如 .json .jpg .txt
+     *
+     * @param file File
+     * @param pathList 存储搜寻到的路径
+     * @param suffix 文件后缀
      */
-    public static void getJsonFilePath(File file, List<String> pathList) {
+    public static void getFilePathStringsBySuffix(File file, List<String> pathList,String suffix) {
         if (file.exists() && file.isDirectory()) {
             for (File file1 : Objects.requireNonNull(file.listFiles())) {
-                if (file1.toString().endsWith(".json")) {
+                if (file1.toString().endsWith(suffix)) {
                     pathList.add(file1.toString());
                     // System.out.println(file1);
                 }
-                getJsonFilePath(file1,pathList);
+                getFilePathStringsBySuffix(file1,pathList,suffix);
             }
         }
+    }
+
+    /**
+     * 根据指定文件路径所在目录搜寻指定同级目录下所有文件，获取其文件名-路径
+     *
+     * 如：
+     * <pre>
+     *     当前文件下存在 ： ./  hello.json  img/
+     *     fromFilePathGetDirectoryFiles("./hello.json","img") 则会返回所有img目录下的文件
+     *
+     * </pre>
+     *
+     * 注: 搜寻的必须是一个同级文件夹
+     *
+     * @param filePath String 指定文件路径
+     * @return Map<String,File> 文件名-路径
+     */
+    public static Map<String,File> fromFilePathGetDirectoryFiles(String filePath,String directoryName){
+        final File file = new File(filePath);
+        Map<String, File> imgNames = new HashMap<>();
+        if (file.exists()){
+            // 路径：  .../img  这里约定img文件夹为下载路径
+            File imgFile = new File(file.getParentFile() + File.separator + directoryName);
+            if (imgFile.isDirectory()){
+                File[] files = imgFile.listFiles();
+                if (null!= files){
+                    for (File f:files) {
+                        imgNames.put(f.getName(), f);
+                    }
+                }
+            }
+        }
+        return imgNames;
     }
 
     /**
@@ -97,5 +135,9 @@ public class FileMan {
                 bot.writeTo(fileOutputStream);
             }
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFilePathStringsBySuffix("",".json"));
     }
 }
