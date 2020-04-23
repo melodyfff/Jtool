5f75b518c91a89d02dead656faea56e17b577d0d
melodyfff <307208327@qq.com>
2020-04-23 17:25:40 +0800

    :smile: 新增guice项目

---
+++ guice/src/main/java/com/xinchen/tool/guice/App.java
@@ -0,0 +1,16 @@
+package com.xinchen.tool.guice;
+
+/**
+ *
+ * guice学习
+ * 官方wiki: https://github.com/google/guice/wiki/GettingStarted
+ *
+ * @author xinchen
+ * @version 1.0
+ * @date 23/04/2020 15:24
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
