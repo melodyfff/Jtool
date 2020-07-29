c8592008a479da6fa19354a4d9232727efc193b7
melodyfff <307208327@qq.com>
2020-07-29 23:31:59 +0800

    :smile: hystrix 工作原理

---
+++ hystrix/README.md
@@ -0,0 +1,6 @@
+# Hystrix
+
+[Hystrix工作原理github官方文档](https://github.com/Netflix/Hystrix/wiki/How-it-Works)
+
+[Hystrix工作原理中文翻译](https://blog.csdn.net/forezp/article/details/75333088)
+
---
+++ hystrix/src/main/java/com/xinchen/tool/hystrix/examples/package-info.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * Hystrix官网例子
+ *
+ *
+ * see <a href="https://github.com/Netflix/Hystrix/wiki/How-To-Use">How-To-Use</a>
+ *
+ * @author Xin Chen (xinchenmelody@gmail.com)
+ * @date Created In 2020/7/29 23:02
+ * @version 1.0
+ */
+package com.xinchen.tool.hystrix.examples;
