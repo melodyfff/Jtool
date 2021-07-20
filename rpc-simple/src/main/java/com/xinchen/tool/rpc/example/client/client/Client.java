023f7aad98d9eb465026aeb77aa6d2ae3b4a0208
melodyfff <307208327@qq.com>
2021-07-20 16:30:59 +0800

    :zap: simple-rpc

--- rpc-simple/src/main/java/com/xinchen/tool/rpc/example/client/client/Client.java
+++ rpc-simple/src/main/java/com/xinchen/tool/rpc/example/client/client/Client.java
@@ -2,6 +2,7 @@ package com.xinchen.tool.rpc.example.client.client;
 
 import com.xinchen.tool.rpc.core.client.RpcProxy;
 import com.xinchen.tool.rpc.example.api.HelloService;
+import com.xinchen.tool.rpc.example.api.Person;
 import com.xinchen.tool.rpc.example.client.RpcClientConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,11 +16,15 @@ class Client {
     ApplicationContext context = new AnnotationConfigApplicationContext(RpcClientConfiguration.class);
     RpcProxy rpcProxy = context.getBean(RpcProxy.class);
 
+    Person person = new Person("Jay", "Zhou");
+
     HelloService helloService = rpcProxy.create(HelloService.class);
     System.out.println(helloService.hello("World"));
+    System.out.println(helloService.hello(person));
 
     HelloService helloServiceVersion = rpcProxy.create(HelloService.class, "sample.hello2");
     System.out.println(helloServiceVersion.hello("World"));
+    System.out.println(helloServiceVersion.hello(person));
 
   }
 }
